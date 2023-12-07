486617d6ddbccc7a0c3f3f43900e12f85c803c9a
KaengEE <yopuiqwer@naver.com>
2023-12-07 09:49:14 +0900

    구글아이디로 로그인/로그아웃

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,71 @@
 import "./App.css";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, googleAuth } from "./firebase";
+import { signInWithPopup, signOut } from "firebase/auth";
 
 function App() {
-  return <div className="App">슈퍼챗</div>;
+  const [user, loading, error] = useAuthState(auth);
+  console.log(user);
+
+  if (loading) {
+    return (
+      <div>
+        <p>로딩중...</p>
+      </div>
+    );
+  }
+  if (error) {
+    return (
+      <div>
+        <p>Error:{error}</p>
+      </div>
+    );
+  }
+  return (
+    <div className="App">
+      <header>
+        <h1>Super Chat💬</h1>
+        {user && <SignOut />}
+      </header>
+      <section>{user ? <div>채팅룸</div> : <SignIn />}</section>
+    </div>
+  );
+}
+
+//로그인 컴포넌트
+function SignIn() {
+  const signInWithGoogle = async () => {
+    try {
+      await signInWithPopup(auth, googleAuth);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <button className="sign-in" onClick={signInWithGoogle}>
+        Sign in with Google
+      </button>
+      <p>🌼 커뮤니티에서 예의를 지켜주세요 🌼</p>
+    </>
+  );
+}
+
+//로그아웃 컴포넌트
+function SignOut() {
+  const logout = async () => {
+    try {
+      await signOut(auth);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <button className="sign-out" onClick={logout}>
+      Sign Out
+    </button>
+  );
 }
 
 export default App;
